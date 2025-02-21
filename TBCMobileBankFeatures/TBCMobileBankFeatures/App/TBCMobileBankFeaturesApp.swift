8e701482214f86d156f6a14d3f53988a34f61ca5
Data <133904834+DataRobakidze@users.noreply.github.com>
2025-02-21 19:45:25 +0400

    First commit

---
+++ TBCMobileBankFeatures/TBCMobileBankFeatures/App/TBCMobileBankFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBCMobileBankFeaturesApp.swift
+//  TBCMobileBankFeatures
+//
+//  Created by Data on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TBCMobileBankFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
